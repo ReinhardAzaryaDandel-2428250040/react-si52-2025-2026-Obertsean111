c6feabd032566429ec82542ccd844cd1fe64f83b
rein <reinhardazaryadandel_2428250040@mhs.mdp.ac.id>
2025-11-22 18:41:50 +0700

    buat addmovie

--- src/pages/movies/AddMovie.tsx
+++ src/pages/movies/AddMovie.tsx
@@ -1,25 +1,66 @@
 import { NavLink } from "react-router"
 import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useState, type ChangeEvent, type FormEvent } from "react";
+import ApiClient from "../../utils/ApiClient";
+
+interface FormMovie {
+    judul : string,
+    tahunRilis : string,
+    sutradara : string
+}
 
 function AddMovie(){
+    const [form, setForm] = useState<FormMovie>({
+        judul : "",
+        tahunRilis : "",
+        sutradara : ""
+    })
+
+    const handleInputChange = (event : ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = event.target
+
+        setForm({
+            ...form,
+            [name] : value
+        })
+    }
+
+    const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        try {
+           const response = await ApiClient.post ('/movie', form)
+           console.log(response); 
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
     return <div>
         <div>
-            <div>
-                <p>Judul : <input type="text" name="Judul" />
-                </p>
-            </div>
-            <div>
-                <p>Tahun Rilis : <input type="integer" name="Tahun Rilis" />
-                </p>
-            </div>
-            <div>
-                <p>Pengarang : <input type="text" name="Pengarang   " />
-                </p>
-            </div>
+            <Form onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="formJudulFilm">
+                    <Form.Label>Judul</Form.Label>
+                    <Form.Control 
+                    value={form.judul}
+                    onChange={handleInputChange}
+                    name="judul" 
+                    type="text" 
+                    placeholder="Judul Film"/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formTahunRilis">
+                    <Form.Label>Tahun Rilis</Form.Label>
+                    <Form.Control value={form.tahunRilis} onChange={handleInputChange} name="tahunRilis" type="text" placeholder="Tahun Rilis"/>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="formSutradara">
+                    <Form.Label>Sutradara</Form.Label>
+                    <Form.Control value={form.sutradara } onChange={handleInputChange} name="sutradara" type="text" placeholder="Sutradara"/>
+                </Form.Group>
 
-
+                <Button type="submit" variant="success">Tambah</Button>
+            </Form>
         </div>
-        <Button variant="btn btn-success">Tambah</Button>
+        
         <p></p>
         <p></p>
 
